338a97cdc63c0fd88a105a5fd71e770513133a17
Alon Farchy <afarchy@virtualmaker.net>
2024-06-06 15:59:10 -0500

    Add TryGet/Initialize/Shutdown

--- ServiceLocator/Packages/com.virtualmaker.servicelocator/Runtime/ServiceLocator.cs
+++ ServiceLocator/Packages/com.virtualmaker.servicelocator/Runtime/ServiceLocator.cs
@@ -6,6 +6,8 @@ namespace VirtualMaker
 {
     public interface IService
     {
+        void Initialize();
+        void Shutdown();
     }
 
     [ExecuteAlways]
@@ -14,19 +16,24 @@ namespace VirtualMaker
         private static ServiceLocator _instance;
 
         private Dictionary<Type, IService> _services = new();
+        private List<IService> _servicesInOrder = new();
 
         private void RegisterServices()
         {
             _instance = this;
             _services.Clear();
+            _servicesInOrder.Clear();
 
             foreach (var service in gameObject.GetComponentsInChildren<IService>())
             {
                 var monoBehaviour = service as MonoBehaviour;
-                if (service is IService s && !_services.TryAdd(s.GetType(), s))
+                if (!_services.TryAdd(service.GetType(), service))
                 {
-                    Debug.LogError($"{s.GetType().Name} attached to {monoBehaviour.name} is already registered to service locator!");
+                    Debug.LogError($"{service.GetType().Name} attached to {monoBehaviour.name} is already registered to service locator!");
+                    continue;
                 }
+
+                _servicesInOrder.Add(service);
             }
         }
 
@@ -51,30 +58,67 @@ namespace VirtualMaker
 
         public static T Get<T>() where T : IService
         {
+            if (TryGet(out T service))
+            {
+                return service;
+            }
+
+            if (_instance == null)
+            {
+                throw new Exception($"Service Locator not found");
+            }
+
+            throw new Exception($"Service {typeof(T)} not found");
+        }
+
+        public static bool TryGet<T>(out T service) where T : IService
+        {
             var instance = GetOrCreate();
 
             if (instance == null)
             {
-                throw new Exception("ServiceLocator not found");
+                // instance can be null if called while stopping play mode
+                // because ServiceLocator is destroyed before other objects.
+                service = default!;
+                return false;
             }
 
-            if (!instance._services.TryGetValue(typeof(T), out var service))
+            if (!instance._services.TryGetValue(typeof(T), out var svc))
             {
                 // Check if the service was just added in editor.
                 if (!Application.isPlaying)
                 {
                     instance.RegisterServices();
 
-                    if (instance._services.TryGetValue(typeof(T), out service))
+                    if (instance._services.TryGetValue(typeof(T), out svc))
                     {
-                        return (T)service;
+                        service = (T)svc;
+                        return true;
                     }
                 }
 
-                throw new Exception($"Service {typeof(T)} not found");
+                service = default!;
+                return false;
             }
 
-            return (T)service;
+            service = (T)svc;
+            return true;
+        }
+
+        private void OnEnable()
+        {
+            foreach (var service in _servicesInOrder)
+            {
+                service.Initialize();
+            }
+        }
+
+        private void OnDisable()
+        {
+            foreach (var service in _servicesInOrder)
+            {
+                service.Shutdown();
+            }
         }
     }
 }
