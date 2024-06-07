ba0619eac92235362b45e791b70ac2637ce73900
Alon Farchy <34091646+afarchy@users.noreply.github.com>
2024-06-07 10:28:28 -0500

    Add TryGet/ServiceEnable/ServiceDisable (#1)

--- ServiceLocator/Packages/com.virtualmaker.servicelocator/Runtime/ServiceLocator.cs
+++ ServiceLocator/Packages/com.virtualmaker.servicelocator/Runtime/ServiceLocator.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,6 +7,8 @@ namespace VirtualMaker
 {
     public interface IService
     {
+        void ServiceEnable();
+        void ServiceDisable();
     }
 
     [ExecuteAlways]
@@ -14,19 +17,24 @@ namespace VirtualMaker
         private static ServiceLocator _instance;
 
         private Dictionary<Type, IService> _services = new();
+        private List<IService> _servicesInOrder = new();
 
         private void RegisterServices()
         {
             _instance = this;
             _services.Clear();
+            _servicesInOrder.Clear();
 
             foreach (var service in gameObject.GetComponentsInChildren<IService>())
             {
                 var monoBehaviour = service as MonoBehaviour;
-                if (service is IService s && !_services.TryAdd(s.GetType(), s))
+                if (!_services.TryAdd(service.GetType(), service))
                 {
-                    Debug.LogError($"{s.GetType().Name} attached to {monoBehaviour.name} is already registered to service locator!");
+                    Debug.LogError($"{service.GetType().Name} attached to {monoBehaviour.name} is already registered to service locator!");
+                    continue;
                 }
+
+                _servicesInOrder.Add(service);
             }
         }
 
@@ -51,30 +59,82 @@ namespace VirtualMaker
 
         public static T Get<T>() where T : IService
         {
+            if (TryGet(out T service))
+            {
+                return service;
+            }
+
+            if (_instance == null)
+            {
+                throw new Exception($"Service Locator not found");
+            }
+
+            throw new Exception($"Service {typeof(T)} not found");
+        }
+
+        public static bool TryGet<T>(out T service) where T : IService
+        {
             var instance = GetOrCreate();
 
             if (instance == null)
             {
-                throw new Exception("ServiceLocator not found");
+                // instance can be null if called while stopping play mode
+                // because ServiceLocator is destroyed before other objects.
+                service = default!;
+                return false;
             }
 
-            if (!instance._services.TryGetValue(typeof(T), out var service))
+            if (!instance._services.TryGetValue(typeof(T), out var knownSvc))
             {
                 // Check if the service was just added in editor.
                 if (!Application.isPlaying)
                 {
                     instance.RegisterServices();
 
-                    if (instance._services.TryGetValue(typeof(T), out service))
+                    if (instance._services.TryGetValue(typeof(T), out var svc))
                     {
-                        return (T)service;
+                        service = (T)svc;
+                        return true;
                     }
                 }
 
-                throw new Exception($"Service {typeof(T)} not found");
+                service = default!;
+                return false;
             }
 
-            return (T)service;
+            service = (T)knownSvc;
+            return true;
+        }
+
+        private void OnEnable()
+        {
+            if (!Application.isPlaying)
+            {
+                return;
+            }
+
+            if (_servicesInOrder.Count == 0)
+            {
+                RegisterServices();
+            }
+
+            foreach (var service in _servicesInOrder)
+            {
+                service.ServiceEnable();
+            }
+        }
+
+        private void OnDisable()
+        {
+            if (!Application.isPlaying)
+            {
+                return;
+            }
+
+            for (int i = _servicesInOrder.Count - 1; i >= 0; i--)
+            {
+                _servicesInOrder[i].ServiceDisable();
+            }
         }
     }
 }
